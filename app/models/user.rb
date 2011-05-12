bd682ea2506d00de8a8bf0362c2857e928dee364
Jay Godse <jgodse@gmail.com>
2011-05-12 12:38:53 -0400

    Added code to save and display user info

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User
   field :uid, :type => String
   field :name, :type => String
   field :email, :type => String
-  attr_accessible :provider, :uid, :name, :email
+  field :user_info, :type=> String
+  attr_accessible :provider, :uid, :name, :email, :user_info
 
   def self.create_with_omniauth(auth)
     begin
@@ -14,10 +15,12 @@ class User
         if auth['user_info']
           user.name = auth['user_info']['name'] if auth['user_info']['name'] # Twitter, Google, Yahoo, GitHub
           user.email = auth['user_info']['email'] if auth['user_info']['email'] # Google, Yahoo, GitHub
+		  user.user_info = auth['user_info']
         end
         if auth['extra']['user_hash']
           user.name = auth['extra']['user_hash']['name'] if auth['extra']['user_hash']['name'] # Facebook
           user.email = auth['extra']['user_hash']['email'] if auth['extra']['user_hash']['email'] # Facebook
+		  user.user_info = auth['extra']
         end
       end
     rescue Exception
