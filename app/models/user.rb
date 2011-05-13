ca4e01dde6d547a7e38c7b1bd1daf4e02e6d1407
Jay Godse <jgodse@gmail.com>
2011-05-13 09:57:45 -0400

    Save user_info to YAML

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,12 +15,12 @@ class User
         if auth['user_info']
           user.name = auth['user_info']['name'] if auth['user_info']['name'] # Twitter, Google, Yahoo, GitHub
           user.email = auth['user_info']['email'] if auth['user_info']['email'] # Google, Yahoo, GitHub
-		  user.user_info = auth['user_info']
+		  user.user_info = auth['user_info'].to_yaml
         end
         if auth['extra']['user_hash']
           user.name = auth['extra']['user_hash']['name'] if auth['extra']['user_hash']['name'] # Facebook
           user.email = auth['extra']['user_hash']['email'] if auth['extra']['user_hash']['email'] # Facebook
-		  user.user_info = auth['extra']
+		  user.user_info = auth['extra'].to_yaml
         end
       end
     rescue Exception
