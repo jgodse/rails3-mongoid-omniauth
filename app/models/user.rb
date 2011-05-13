cc389b8b48f9b2c7bb76d26a01c65c49e924259c
Jay Godse <jgodse@gmail.com>
2011-05-13 12:14:39 -0400

    Try saving user_info as JSON

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
+require 'json'
 class User
   include Mongoid::Document
   field :provider, :type => String
@@ -15,12 +16,12 @@ class User
         if auth['user_info']
           user.name = auth['user_info']['name'] if auth['user_info']['name'] # Twitter, Google, Yahoo, GitHub
           user.email = auth['user_info']['email'] if auth['user_info']['email'] # Google, Yahoo, GitHub
-		  user.user_info = auth['user_info'].to_yaml
+		  user.user_info = auth['user_info'].to_json
         end
         if auth['extra']['user_hash']
           user.name = auth['extra']['user_hash']['name'] if auth['extra']['user_hash']['name'] # Facebook
           user.email = auth['extra']['user_hash']['email'] if auth['extra']['user_hash']['email'] # Facebook
-		  user.user_info = auth['extra'].to_yaml
+		  user.user_info = auth['extra'].to_json
         end
       end
     rescue Exception
