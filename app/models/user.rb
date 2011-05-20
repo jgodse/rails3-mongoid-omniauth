2125e089d8296c22b0962afe2f74e70434d40cf4
Jay Godse <jgodse@gmail.com>
2011-05-20 14:06:43 -0400

    Use different way to create user

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,24 +10,43 @@ class User
 
   def self.create_with_omniauth(auth)
 	#raise Exception, auth.inspect
-    begin
-      create! do |user|
-        user.provider = auth['provider']
-        user.uid = auth['uid']
-        if auth['user_info']
-          user.name = auth['user_info']['name'] if auth['user_info']['name'] # Twitter, Google, Yahoo, GitHub
-          user.email = auth['user_info']['email'] if auth['user_info']['email'] # Google, Yahoo, GitHub
-		  user.user_info = auth['user_info'].to_json
-        end
-        if auth['extra']['user_hash']
-          user.name = auth['extra']['user_hash']['name'] if auth['extra']['user_hash']['name'] # Facebook
-          user.email = auth['extra']['user_hash']['email'] if auth['extra']['user_hash']['email'] # Facebook
-		  user.user_info = auth['extra'].to_json
-        end
-      end
-    rescue Exception
-      raise Exception, "cannot create user record" + auth.inspect
+
+	user = User.new
+	user.provider = auth['provider']
+	user.uid = auth['uid']
+	if auth['user_info']
+	  user.name = auth['user_info']['name'] if auth['user_info']['name'] # Twitter, Google, Yahoo, GitHub
+	  user.email = auth['user_info']['email'] if auth['user_info']['email'] # Google, Yahoo, GitHub
+	  user.user_info = auth['user_info'].to_json#
     end
+	if auth['extra']['user_hash']
+	  user.name = auth['extra']['user_hash']['name'] if auth['extra']['user_hash']['name'] # Facebook
+	  user.email = auth['extra']['user_hash']['email'] if auth['extra']['user_hash']['email'] # Facebook
+	  user.user_info = auth['extra'].to_json
+	end
+	
+	if !user.save
+		raise Exception, "cannot create user record: " + user.errors.inspect
+	end
+	user
+#    begin
+#      create! do |user|
+#        user.provider = auth['provider']
+#        user.uid = auth['uid']
+#        if auth['user_info']
+#          user.name = auth['user_info']['name'] if auth['user_info']['name'] # Twitter, Google, Yahoo, GitHub
+#         user.email = auth['user_info']['email'] if auth['user_info']['email'] # Google, Yahoo, GitHub
+#		  user.user_info = auth['user_info'].to_json#
+#        end
+#        if auth['extra']['user_hash']
+#          user.name = auth['extra']['user_hash']['name'] if auth['extra']['user_hash']['name'] # Facebook
+#          user.email = auth['extra']['user_hash']['email'] if auth['extra']['user_hash']['email'] # Facebook
+#		  user.user_info = auth['extra'].to_json
+#        end
+#      end
+#    rescue Exception
+#      raise Exception, "cannot create user record" + auth.inspect
+#    end
   end
 
 end
